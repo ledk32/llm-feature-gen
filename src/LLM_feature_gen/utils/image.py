5e3d2459002cfac18e597a7961859f72164a3064
JuliaYershova <potip.julia@gmail.com>
2026-03-06 16:04:03 +0100

    preprocess image

--- src/LLM_feature_gen/utils/image.py
+++ src/LLM_feature_gen/utils/image.py
@@ -3,8 +3,11 @@ import base64, io
 from PIL import Image
 import numpy as np
 
-def image_to_base64(img_arr: np.ndarray) -> str:
+
+def image_to_base64(img_arr: np.ndarray, max_size: int = 384, quality: int = 85) -> str:
     img = Image.fromarray(img_arr)
+    img = img.convert("RGB")
+    img.thumbnail((max_size, max_size))
     buf = io.BytesIO()
-    img.save(buf, format="PNG")
+    img.save(buf, format="JPEG", quality=quality, optimize=True)
     return base64.b64encode(buf.getvalue()).decode("utf-8")
